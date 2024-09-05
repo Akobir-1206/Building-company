ac6e981eab905ebaa28873d6a7ac6348fc572094
Akobir-1206 <akobiryuldashaliyev9@gmail.com>
2024-09-05 20:57:47 +0500

    777

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, vero.</h1>
+    </div>
+  )
+}
